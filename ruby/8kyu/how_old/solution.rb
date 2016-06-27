2c431293cd8ae574f90617e9ce899ce2bea8d7af
Chris | PhO3n1x <voelkel.chris@googlemail.com>
2016-06-27 21:31:48 +1200

    Modify Ruby Kata 'How old will you be in ...'

--- ruby/8kyu/how_old/solution.rb
+++ ruby/8kyu/how_old/solution.rb
@@ -1,7 +1,9 @@
-def shortcut(s)
-
-  # Use String#gsub to find all lowercase vowels (RegExp)
-  # and replace them with ''
-  s.gsub(/[aeiou]/, '')
-  
+def calculate_age(year_of_birth, current_year)
+  if year_of_birth < current_year
+    (current_year - year_of_birth) > 1 ? "You are #{current_year - year_of_birth} years old." : "You are 1 year old."
+  elsif year_of_birth > current_year
+    (year_of_birth - current_year) > 1 ? "You will be born in #{year_of_birth - current_year} years." : "You will be born in 1 year."
+  else
+    'You were born this very year!'
+  end
 end
